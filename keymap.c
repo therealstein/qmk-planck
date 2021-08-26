9a640d153cae02a322d46fabf2cc7dfb18f9d1c8
Steffen Stein <therealstein@tutanota.com>
2021-08-26 13:49:33 +0200

    change

--- keymap.c
+++ keymap.c
@@ -22,6 +22,8 @@ enum unicode_names {
     OE,
     UE,
     SZ,
+    CRIGHT,
+    EURO
 };
 
 const uint32_t PROGMEM unicode_map[] = {
@@ -31,14 +33,16 @@ const uint32_t PROGMEM unicode_map[] = {
     [AE]=0x00E4,
     [OE]=0x00F6,
     [UE]=0x00FC,
-    [SZ]=0x00DF
+    [SZ]=0x00DF,
+    [CRIGHT]=0x00A9,
+    [EURO]=0x20AC
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FUNCY1 LT(_FUNCY1, KC_TAB)
-#define FUNCY2 MO(_FUNCY2)
-#define BSPWM MO(_BSPWM)
+#define FUNCY2 LT(_FUNCY2, KC_QUOT)
+#define BSPWM LT(_BSPWM, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -53,10 +57,10 @@ _______,   _______,   _______,   _______,   _______,   _______,   _______,   ___
 
 [_QWERTY] = LAYOUT_planck_grid(
 
-KC_ESC,   KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSPC,
-FUNCY1,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
-KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
-KC_LCTL,   KC_LCTL,   KC_LGUI,   KC_LALT,   LOWER,     FUNCY2,    KC_SPC,    RAISE,     KC_RALT,   BSPWM,     KC_RCTL,   KC_ENT
+BSPWM,   KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSPC,
+FUNCY1,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   FUNCY2,
+KC_LGUI,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_LALT,
+KC_LCTL,   _______,   _______,   _______,   LOWER,   KC_LSFT,     KC_SPC,    RAISE,     _______,   _______,     _______,   KC_ENT
 
 ),
 
@@ -64,17 +68,17 @@ KC_LCTL,   KC_LCTL,   KC_LGUI,   KC_LALT,   LOWER,     FUNCY2,    KC_SPC,    RAI
 
 KC_TILD,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,   _______,
 KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,   _______,
-KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,   _______,   _______,   KC_HOME,   KC_END,    _______,
+KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,   _______,   _______,   _______,   _______,    _______,
 _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______
 
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
 
-KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      _______,
-KC_DEL,    _______,   _______,   _______,   _______,   _______,   _______,   KC_MINS,   KC_EQL,    _______,   _______,   _______,
-_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_PGUP,   KC_PGDN,   _______,
-_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_MNXT,   KC_VOLD,   KC_VOLU,   KC_MPLY
+KC_PSCR,   KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      _______,
+_______,   KC_INS,    KC_HOME,   KC_PGUP,   _______,   _______,   _______,   KC_4,      KC_5,      KC_6,      _______,   _______,
+_______,   KC_DEL,    KC_END,    KC_PGDN,   _______,   _______,   _______,   KC_1,      KC_2,      KC_3,      KC_PGDN,   _______,
+_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_0,      KC__VOLUP, KC__VOLDOWN, KC__MUTE
 
 ),
 
@@ -89,10 +93,10 @@ _______,   _______,   _______,   _______,   _______,   _______,   _______,   ___
 
 [_FUNCY1] = LAYOUT_planck_grid(
 
-_______,   _______,   _______,   _______,   _______,   KC_GRAVE,    KC_BSLS,  _______,   _______,   _______,   _______,   _______,
-_______,   _______,   KC_LPRN,   KC_LCBR,   KC_LBRC,   _______,    _______,  KC_RBRC,   KC_RCBR,   KC_RPRN,   _______,   _______,
-_______,   _______,   _______,   _______,   KC_LT,     _______,    _______,  KC_GT,     _______,   _______,   _______,   _______,
-_______,   _______,   _______,   _______,   _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______
+_______,   _______,   _______,   _______,   _______,   G(KC_GRAVE),KC_BSLS,     _______,   _______,   _______,   _______,   _______,
+_______,   _______,   KC_LPRN,   KC_LCBR,   KC_LBRC,   KC_GRAVE,   G(KC_BSLS),  KC_RBRC,   KC_RCBR,   KC_RPRN,   _______,   _______,
+_______,   _______,   _______,   _______,   KC_LT,     G(KC_EQL),  KC_EQL,      KC_GT,     _______,   _______,   _______,   _______,
+_______,   _______,   _______,   _______,   _______,   KC_MINS,    G(KC_MINS),  _______,   _______,   _______,   _______,   _______
 
 ),
 
@@ -106,10 +110,10 @@ XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   G(KC_PSCR),KC_PSCR,   XXX
 ),
 
 [_FUNCY2] = LAYOUT_planck_grid(
-_______,   X(SNEK),      _______,   _______,   _______,   _______,   _______,   X(UE),     _______,     X(OE),   _______,   _______,
-_______,   X(AE),        X(SZ),     _______,   _______,   _______,    KC_MS_L,  KC_MS_D,   KC_MS_U,   KC_MS_R,   _______, _______,  
-_______,   X(BANG),      _______,   _______,   _______,   _______,   _______,   KC_WH_D,   KC_WH_U,   _______,   _______,   _______, 
-_______,   X(IRONY),      _______,   _______,   _______,   _______,   KC_BTN1,   KC_BTN2,   _______,   _______,   _______,   _______ 
+_______,   X(SNEK),      _______,   X(EURO),   _______,   _______,   _______,   X(UE),     _______,     X(OE),   _______,   _______,
+_______,   X(AE),        X(SZ),     _______,   _______,   _______,   _______,   _______,  _______,   _______,   _______, _______,  
+_______,   X(BANG),      _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,   _______,   _______, 
+_______,   X(IRONY),      _______,  X(CRIGHT),   _______,   _______,   _______,   _______,    _______,   _______,   _______,   _______ 
 ),
 
 
